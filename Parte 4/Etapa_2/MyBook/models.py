00f4c47c51746d50b59623e7ece683e70774a874
Max <max.antonio1703.14@gmail.com>
2023-09-24 17:27:10 -0300

    modelo e migrações

--- Parte 4/Etapa_2/MyBook/models.py
+++ Parte 4/Etapa_2/MyBook/models.py
@@ -14,7 +14,7 @@ class Livro(models.Model):
     quantidade_leitores = models.IntegerField(null=False) #não nulo
     existe_no_sistema = models.BooleanField(null=False) #não nulo
     id_editora = models.ForeignKey(Editora, on_delete=models.CASCADE) #chave estrangeira, ao deletar a editora deleta todos os livros associados a ela
-    id_admnistrador = models.IntegerField(Administrador, on_delete=models.SET(-1)) #chave estrangeira, eu deletar administrador seta valor para -1
+    id_admnistrador = models.ForeignKey(Administrador, on_delete=models.SET(-1)) #chave estrangeira, eu deletar administrador seta valor para -1
 
 class Leitor(models.Model):
     cpf = models.CharField(primary_key=True, null=False, max_length=11) #chave primária, não nulo
@@ -25,9 +25,15 @@ class Leitor(models.Model):
     apelido = models.CharField(null=False, max_length=45) #não nulo
 
 class Livros_Leitor(models.Model):
-    cpf = models.ForeignKey(Leitor, primary_key=True, on_delete=models.CASCADE) #chave estrangeira, chave primária
-    ISBN = models.ForeignKey(Livro, primary_key=True, on_delete=models.CASCADE) #chave estrangeira, chave primária
+    cpf = models.ForeignKey(Leitor, on_delete=models.CASCADE) #chave estrangeira, chave primária
+    ISBN = models.ForeignKey(Livro, on_delete=models.CASCADE) #chave estrangeira, chave primária
     livro_lido = models.CharField(max_length=45)
     livro_favorito = models.CharField(max_length=45)
     livro_em_progresso = models.CharField(max_length=45)
     livro_pretende_ler = models.CharField(max_length=45)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields = ['cpf', 'ISBN'], name="cpf_e_ISBN"
+            )
+        ]
